a16a25a2692e797bb6fe5ca6b449370ea2ba1c3c
iseb <sabastiandanbaibe@esp.sn>
2023-06-20 00:55:06 +0000

    modification gitignore et pom.xml (maven plugin)

--- src/main/java/com/infolog/InfoloServices/model/Client.java
+++ src/main/java/com/infolog/InfoloServices/model/Client.java
@@ -37,7 +37,7 @@ public class Client {
             cascade = CascadeType.ALL,
             orphanRemoval = true
     )
-    private List<Account> accounts = new ArrayList<>();
+    private List<com.infolog.InfologServices.model.Account> accounts = new ArrayList<>();
 
     public List<Account> getAccounts() {
         return accounts;
